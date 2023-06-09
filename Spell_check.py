6ce35f9bc5fbe2ae1f4d30ad0fc6328f4558ed6b
Ved Prakash Mishra <119031449+vpm743@users.noreply.github.com>
2023-06-09 17:30:03 +0530

    Add files via upload

---
+++ Spell_check.py
@@ -0,0 +1,14 @@
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+def correct_word(word):
+    # This part of code check if the word is misspelled
+    if not spell.correction(word) == word:
+        return spell.correction(word)
+    else:
+        # This part is for if the word is not misspelled, find the nearest words
+        return spell.candidates(word)
+    
+enter_word=input("Enter Word Here :- ")
+print(correct_word(enter_word))
